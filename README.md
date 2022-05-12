008effb13b866ab0fcdb3982c3779e5d3cec0f61
Marcia <mwan0906@gmail.com>
2022-05-12 15:54:52 -0400

    Initial app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tic-tac-toe
+# General Architecture
+This is a single page application written in React, with Node.js on the backend, ran on localhost:3000.
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+"use strict";
+const path = require('path');
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  entry: './client/index.js',
+  mode: isDev ? 'development' : 'production',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module : {
+    rules : [
+      {
+        test : /.jsx?$/,
+        loader : 'babel-loader',
+        options : { presets : ['@babel/react']}
+
+      }
+    ]
+  },
+  devtool : 'source-map'
+};
